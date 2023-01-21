8ca3dda585aa9cf6d73054d0ef571de4865bd5b6
juan jose londoño <juanjoselondonodavid@gmail.com>
2023-01-20 23:38:13 -0500

    axios added / change adaptative icon

--- components/Quotes.js
+++ components/Quotes.js
@@ -1,26 +1,15 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 export default function Quotes() {
   const [quote, setQuote] = useState(1)
   useEffect(()=>{
     const getQuote = async()=>{
-        let options = {
-            method: 'GET',
-            headers: { 'x-api-key': 'g4L5zU1CL1R0HwV/zxDnUQ==XPLhQtsl5vebl8IK' }
-        }
-        
-        let url = 'https://api.api-ninjas.com/v1/quotes?category=inspirational'
-        
         try{
-            await  fetch(url,options)
-            .then(res => res.json()) // parse response as JSON
-            .then(data => {
-                console.log(data)
-                setQuote(data[0])
+            await axios.get('https://zenquotes.io/api/quotes').then((data)=>{
+                console.log(data.data.slice(0,1))
+                setQuote(data.data[0])
             })
-            .catch(err => {
-                console.log(`error ${err}`)
-            }); 
         }
         catch(err){
             console.log(err)
@@ -37,8 +26,8 @@ export default function Quotes() {
         {
             quote != 1 &&
             <View style = {styles.container}>
-                <Text style = {styles.quote}>“{quote.quote}”</Text>
-                <Text style = {styles.author}>{'-' + quote.author}</Text>
+                <Text style = {styles.quote}>“{quote.q}”</Text>
+                <Text style = {styles.author}>{'-' + quote.a}</Text>
             </View>
         }
     </View>
